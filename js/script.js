a717af50ca97043d99012902e0628fbe79199e10
faberfff <faverfff@gmail.com>
2022-12-06 09:04:17 -0500

    :sparkles: se crea el archivo con los archivos indicados en la asignatura

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//java script se creo para la manipulacion del dom, para las animaciones, sliders
+
+const foot = document.getElementById('year');
+const year = new Date().getFullYear();
+foot.innerHTML = year;
+// estas tres lineas de codigo muestran en el footer el año actual sin necesidad de estarlo cambiando cadavez que cambie
+
